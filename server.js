1273be725561857de61358b95d046fba71f2eb6c
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-02-10 20:08:39 +0300

    Add start

--- server.js
+++ server.js
@@ -12,4 +12,6 @@ const worker = function (req, res) {
 const server = http.createServer(worker);
 
 console.log('Сервер запущен!');
-server.listen(3000);
+const port = process.env.PORT || 3000;
+
+server.listen(port);
