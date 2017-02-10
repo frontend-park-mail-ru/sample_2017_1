fccd4424f5ce6f1cc4ad794711dffe6ed70cc996
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-02-10 20:40:29 +0300

    Add start

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const http = require('http');
 const fs = require('fs');
 
 const worker = function (req, res) {
-	const text = fs.readFileSync('./static/index.html', 'utf8');
+
+	const url = req.url;
+	console.log('GET ' + url);
+
+	let text;
+	if (url === '/') {
+		text = fs.readFileSync('./static/index.html', 'utf8');
+	} else {
+		text = fs.readFileSync('./static/hello.html', 'utf8');
+	}
 	res.write(text);
 	res.end();
 };
