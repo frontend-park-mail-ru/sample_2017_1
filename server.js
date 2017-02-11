c2e3e9fe219cd7cbcd7eb675e174356f5a76b179
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-02-11 15:15:44 +0300

    Add comments

--- server.js
+++ server.js
@@ -3,24 +3,25 @@
 const http = require('http');
 const fs = require('fs');
 
-const worker = function (req, res) {
+const worker = function (request, response) {
+	console.log(`${request.method} ${request.url}`);
+	const url = request.url;
 
-	const url = req.url;
-	console.log('GET ' + url);
-
-	let text;
+	let content;
 	if (url === '/') {
-		text = fs.readFileSync('./static/index.html', 'utf8');
+		content = fs.readFileSync('./static/index.html', 'utf8');
 	} else {
-		text = fs.readFileSync('./static/hello.html', 'utf8');
+		content = fs.readFileSync('./static/hello.html', 'utf8');
 	}
-	res.write(text);
-	res.end();
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(content);
+	response.end();
+	console.log('complete');
 };
 
 const server = http.createServer(worker);
 
-console.log('Сервер запущен!');
 const port = process.env.PORT || 3000;
+console.log(`Сервер запущен! Порт ${port}`);
 
 server.listen(port);
