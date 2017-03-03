6e47a209f74539f88d93146841949041c6f7b5f6
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-03-03 17:44:53 +0300

    Add online-chat

--- server.js
+++ server.js
@@ -1,54 +1,14 @@
 'use strict';
 
-// Импортируем модули, отвечающие за работу с http и с файловой системой
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const app = express();
 
 const BASE_PATH = 'static';
-
-// Создаём фукнцию-обработчик запросов
-const worker = function (request, response) {
-	// Выводим лог
-	console.log(`${request.method} ${request.url}`);
-	const url = request.url;
-
-	// Определяем, какой из файлов мы будем использовать
-	let content;
-	// мы оптимисты
-	let statusCode = 200;
-
-	if (url === '/') {
-		content = fs.readFileSync(`${BASE_PATH}/index.html`, 'utf8');
-	} else {
-		let filePath = path.resolve(`${BASE_PATH}${url}`); // наш файл лежит здесь
-		try {
-			fs.accessSync(filePath); // проверим!
-		} catch (e) {
-			statusCode = 404;
-			content = e.toString();
-		}
-
-		if (!content) {
-			content = fs.readFileSync(filePath, 'utf8');
-		}
-	}
-
-	// Записываем статус в ответ
-	response.statusCode = statusCode;
-
-	// Данные в ответ
-	response.write(content);
-	response.end();
-	console.log('complete');
-};
-
-// Создаём сервер
-const server = http.createServer(worker);
+app.use('/', express.static(BASE_PATH));
 
 // Определяем, соединения на каком порту будет обрабатывать сервер
 const port = process.env.PORT || 3000;
 console.log(`Сервер запущен! Порт ${port}`);
 
 // Запускаем сервер
-server.listen(port);
+app.listen(port);
