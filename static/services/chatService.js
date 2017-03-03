8e3b6eea77c20ab64b2ec45be54ccd25cca8e16a
Anatoliy Ostapenko <a.ostapenko@corp.mail.ru>
2017-03-03 17:53:01 +0300

    Prepare class branch

--- static/services/chatService.js
+++ static/services/chatService.js
@@ -4,28 +4,7 @@
 	const HTTP = window.HTTP;
 
 	class ChatService {
-		constructor() {
-			this.http = new HTTP();
-		}
 
-		sendMessage(text, login, email, callback) {
-			const body = {text, login, email};
-			this.http.post('/messages', body, xhr => {
-				callback.call(xhr, JSON.parse(xhr.responseText || ''));
-			});
-		}
-
-		getMessages(callback) {
-			this.http.get('/messages', null, xhr => {
-				callback.call(xhr, JSON.parse(xhr.responseText || ''));
-			});
-		}
-
-		removeAllMessages(callback) {
-			this.http.delete('/messages', null, xhr => {
-				callback.call(xhr, JSON.parse(xhr.responseText || ''));
-			});
-		}
 	}
 
 	window.ChatService = ChatService;
